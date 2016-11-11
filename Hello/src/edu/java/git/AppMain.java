c3222567da07f50c6a1cac9ce184cda1c5748ffb
dkfldk14 <dkfldk14@gmail.com>
2016-11-11 14:49:25 +0900

    Update AppMain.java

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -5,7 +5,7 @@ public class AppMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-			System.out.println("hello, world!");
+			System.out.println("hello, Git!");
 			
 			System.out.println("¼öÁ¤.. Ãß°¡µÊ");
               System.out.println("--");
