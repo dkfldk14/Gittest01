de4b4db731d4e4409dec4b24ed74dcd4b3117b00
dkfldk14 <dkfldk14@gmail.com>
2016-11-11 14:49:47 +0900

    다시고침

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -5,7 +5,7 @@ public class AppMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-			System.out.println("hello, Git!");
+			System.out.println("hello, Git010!");
 			
 			System.out.println("¼öÁ¤.. Ãß°¡µÊ");
               System.out.println("--");
